e2fe6f847d4c37d376c2578ff7e043fd7e5aa9c1
Bazinga Bazinga <matvei2in@gmail.com>
2021-08-29 08:55:43 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepozitory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepozitory : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1,"King Arture"),
+            new Book(2, "King Leo"),
+            new Book(3, "Big Elephant")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
